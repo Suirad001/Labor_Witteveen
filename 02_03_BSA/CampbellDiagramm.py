5748eb605ea23ad2e1bf19efefab516a01cdf92f
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2025-02-07 15:55:04 +0100

    Zeitvektor über Abtastfrequenz bestimmt

--- 02_03_BSA/CampbellDiagramm.py
+++ 02_03_BSA/CampbellDiagramm.py
@@ -19,5 +19,18 @@ a3 = pd.read_excel("./02_03_BSA/a3_r5.xlsx", header = None).iloc[:, 1]
 
 # Zeitvektor
 #-----------
-time = pd.read_excel("./02_03_BSA/a0_1r.xlsx", header = None).iloc[:, 0]
+fAbtast = 1.6516129 * 10**3
+dt = 1 / fAbtast
+nMeas = len(a0)
+
+time = np.arange(0, nMeas, dt)
+
+
+# Drehzahl des Motors
+#--------------------
+def getRot(U):
+    # Lineare Interpolation zw. 2 Messwerten
+    n = 572.142857 * U - 93
+    return n
+
 
