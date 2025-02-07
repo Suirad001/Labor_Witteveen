75e38b127f7e3f43e840242990e4966023a13053
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2025-02-07 15:12:11 +0100

    restliche Beschl. + Zeit eingelesen

--- 02_03_BSA/CampbellDiagramm.py
+++ 02_03_BSA/CampbellDiagramm.py
@@ -9,7 +9,15 @@ import pandas as pd
 
 # 1 - Einlesen der Messdaten
 #===========================
-a0 = pd.read_excel("./02_03_BSA/a0_1r.xlsx", engine = "openpyxl").iloc[:, 1]
 
+# Beschleunigungen der Messpunkte
+#--------------------------------
+a0 = pd.read_excel("./02_03_BSA/a0_1r.xlsx", header = None).iloc[:, 1]
+a1 = pd.read_excel("./02_03_BSA/a1_r2.xlsx", header = None).iloc[:, 1]
+a2 = pd.read_excel("./02_03_BSA/a2_r4.xlsx", header = None).iloc[:, 1]
+a3 = pd.read_excel("./02_03_BSA/a3_r5.xlsx", header = None).iloc[:, 1]
 
-print(a0)
+# Zeitvektor
+#-----------
+time = pd.read_excel("./02_03_BSA/a0_1r.xlsx", header = None).iloc[:, 0]
+
