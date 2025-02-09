3cdd445faeac523b14e8837e07c1b1ae1125766c
Erik0807 <erik.grosshaupt@students.fh-wels.at>
2025-02-09 09:37:41 +0100

    Aufteilung eines Vektors

--- 02_03_BSA/CampbellDiagramm.py
+++ 02_03_BSA/CampbellDiagramm.py
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 #--------------------------------
 a0 = pd.read_excel("./02_03_BSA/a0_1r.xlsx", header = None).iloc[1:, 1]
 a0 = np.array(a0)
+a0.astype(float)
 
 a1 = pd.read_excel("./02_03_BSA/a1_r2.xlsx", header = None).iloc[1:, 1]
 a1 = np.array(a1)
@@ -54,3 +55,14 @@ U = np.linspace(Ustart, Uend, nMeas)
 n = getRot(U)
 #============================================================================
 
+# 2 - Transformation in den Freq.-Bereich
+#========================================
+
+# Funktion zum Aufsplitten eines Vektors
+#---------------------------------------
+def splitVec(vec, size):
+    return np.array_split(vec, np.ceil(len(time) / size))
+
+# Unterteilen des Zeitvektors
+#----------------------------
+timeSplit = splitVec(time, 100)
